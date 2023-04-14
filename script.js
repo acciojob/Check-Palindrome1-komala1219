3b2590aa76c1692812a8173d82699458d3e6d345
komala1219 <123307320+komala1219@users.noreply.github.com>
2023-04-14 16:10:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,16 @@
 
 function palindrome(str){
 
+	str = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  let left = 0, right = str.length - 1;
+  while (left < right) {
+    if (str.charAt(left) !== str.charAt(right)) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
+
 }
 module.exports = palindrome
